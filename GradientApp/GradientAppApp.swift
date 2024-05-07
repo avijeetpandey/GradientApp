97d1a48b8bf62a27cc5ef1bbf597eb0af62d51b3
avijeet <avijeetpandey25@gmail.com>
2024-05-07 15:38:00 +0530

    Initial Commit

---
+++ GradientApp/GradientAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientAppApp.swift
+//  GradientApp
+//
+//  Created by Avijeet on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GradientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
